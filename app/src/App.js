ea65d2810fa08fa7d7e39b03ba832122de4bb9c7
Dana-Sultan <sultand148@gmail.com>
2022-06-11 14:28:23 +0300

    show image

--- app/src/App.js
+++ app/src/App.js
@@ -1,67 +1,105 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import axios from 'axios'
 
-import Button from '@material-ui/core/Button';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
-import IconButton from '@material-ui/core/IconButton';
-import './App.css';
+// import PhotoCamera from '@material-ui/icons/PhotoCamera';
+// import IconButton from '@material-ui/core/IconButton';
 
-function App() {
-  return (
-    // <div className="App">
+
+// function App() {
+
+//   const res = axios.post('/prediction')
+
+//   return (
+//     // <div className="App">
     
-    //   <header className="App-header">
+//     //   <header className="App-header">
         
-    //     <h1 className="App-h1">Caloriephone</h1>
-    //     <button>upload photo</button>
-    //     <input
-    //     type="file"
-    //     accept="image/*"
-    //     style={{ display: 'none' }}
-    //     id="contained-button-file"
-    //   />
-    //   </header>
-    // </div>
-    <div className="App">
+//     //     <h1 className="App-h1">Caloriephone</h1>
+//     //     <button>upload photo</button>
+//     //     <input
+//     //     type="file"
+//     //     accept="image/*"
+//     //     style={{ display: 'none' }}
+//     //     id="contained-button-file"
+//     //   />
+//     //   </header>
+//     // </div>
+//     <div className="App">
       
-    <div style={{
-      display: 'flex',
-      margin: 'auto',
-      width: 400,
-      flexWrap: 'wrap',
-      flexDirection: 'column'
-    }}>
-      <div style={{ width: '100%', float: 'left' }}>
-        <h3>Caloriephone  </h3> <br />
-      </div>
-      <div>
-        <div>
-      <input
-        type="file"
-        accept="image/*"
-        style={{ display: 'none' }}
-        id="contained-button-file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" color="primary" component="span">
-          Upload
-        </Button>
-      </label>
-      </div>
-      <h3>  OR  </h3>
-      <div>
-      <input accept="image/*" id="icon-button-file"
-        type="file" style={{ display: 'none' }} />
-      <label htmlFor="icon-button-file">
-        <IconButton color="primary" aria-label="upload picture"
-        component="span">
-          <PhotoCamera />
-        </IconButton>
-      </label>
-      </div>
-      </div>
-    </div>
-    </div>
-  );
-}
+    
+//     </div>
+//   );
+// }
 
+// export default App;
+
+
+import React, { useState } from "react";
+import './App.css';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
+  
+function App() {
+    const [file, setFile] = useState();
+    const [data, setData] = useState();
+    async function getPrediction() {
+      const res = await axios.post('/predict', file)
+      // set the lable and calorie value
+      setData();
+    }
+    function handleChange(e) {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+
+    }
+  
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1 >Caloriephone</h1>
+            <div className="content">
+            
+
+            <div style={{
+            display: 'flex',
+            margin: 'auto',
+            width: 400,
+            flexWrap: 'wrap',
+            flexDirection: 'column'
+          }}>
+            
+            
+            <div>
+              <div>
+            <input
+              type="file"
+              accept="image/*"
+              style={{ display: 'none' }}
+              id="contained-button-file"
+              onChange={handleChange}
+            />
+            <label htmlFor="contained-button-file">
+              <Button variant="contained" color="primary" component="span">
+                Upload
+              </Button>
+            </label>
+            </div>
+            </div>
+            <image></image>
+          </div>
+          <div className="imageWrap">
+          <img src={file} className="Image"/>
+          </div>
+          </div>
+          <div>
+            data
+          </div>
+          </header>
+
+          
+        </div>
+  
+    );
+}
+  
 export default App;
