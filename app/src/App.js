d35a3e8f6221011c4d09cdc540ffe4f0f4b47afb
Vlad Poberezhny <vlad.pbr@gmail.com>
2022-06-11 21:15:46 +0300

    completed frontend

--- app/src/App.js
+++ app/src/App.js
@@ -1,105 +1,115 @@
-// import logo from './logo.svg';
-// import axios from 'axios'
-
-// import PhotoCamera from '@material-ui/icons/PhotoCamera';
-// import IconButton from '@material-ui/core/IconButton';
-
-
-// function App() {
-
-//   const res = axios.post('/prediction')
-
-//   return (
-//     // <div className="App">
-    
-//     //   <header className="App-header">
-        
-//     //     <h1 className="App-h1">Caloriephone</h1>
-//     //     <button>upload photo</button>
-//     //     <input
-//     //     type="file"
-//     //     accept="image/*"
-//     //     style={{ display: 'none' }}
-//     //     id="contained-button-file"
-//     //   />
-//     //   </header>
-//     // </div>
-//     <div className="App">
-      
-    
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
-import Button from '@material-ui/core/Button';
+import { AppBar, Button, ThemeProvider, Input, Checkbox, FormGroup, FormControlLabel } from "@material-ui/core";
+import { createMuiTheme } from "@material-ui/core";
 import axios from 'axios';
-  
-function App() {
-    const [file, setFile] = useState();
-    const [data, setData] = useState();
-    async function getPrediction() {
-      const res = await axios.post('/predict', file)
-      // set the lable and calorie value
-      setData();
-    }
-    function handleChange(e) {
-        console.log(e.target.files);
-        setFile(URL.createObjectURL(e.target.files[0]));
 
-    }
-  
-    return (
-        <div className="App">
-          <header className="App-header">
-            <h1 >Caloriephone</h1>
-            <div className="content">
-            
+function PredictionList(props) {
 
-            <div style={{
-            display: 'flex',
-            margin: 'auto',
-            width: 400,
-            flexWrap: 'wrap',
-            flexDirection: 'column'
-          }}>
-            
-            
-            <div>
-              <div>
-            <input
-              type="file"
-              accept="image/*"
-              style={{ display: 'none' }}
-              id="contained-button-file"
-              onChange={handleChange}
-            />
-            <label htmlFor="contained-button-file">
-              <Button variant="contained" color="primary" component="span">
-                Upload
-              </Button>
-            </label>
-            </div>
-            </div>
-            <image></image>
-          </div>
-          <div className="imageWrap">
-          <img src={file} className="Image"/>
-          </div>
-          </div>
-          <div>
-            data
-          </div>
-          </header>
+  const [total, setTotal] = useState(JSON.parse(JSON.stringify(props.predictions)));
 
-          
-        </div>
-  
-    );
+  useEffect(() => {
+    setTotal(props.predictions);
+  }, [props.predictions]);
+
+  return (
+    <div>
+      {typeof (props.predictions) !== 'undefined' &&
+        <div>
+          {typeof (total) !== 'undefined' &&
+            <h3>Total: {Object.keys(total).map(label => { return total[label] }).reduce((a, b) => a + b, 0)} cal</h3>
+          }
+          <FormGroup>
+            {
+              Object.keys(props.predictions).map(prediction_label => {
+                return (
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked color="primary" />}
+                    label={prediction_label.charAt(0).toUpperCase() + prediction_label.slice(1).replace("_", " ") + " (" + props.predictions[prediction_label] + " cal)"}
+                    onChange={e => {
+                      let newtotal = {...total}
+                      if (e.target.checked) {
+                        newtotal[prediction_label] = props.predictions[prediction_label]
+                      } else {
+                        newtotal[prediction_label] = 0
+                      }
+                      setTotal(newtotal)
+                    }}
+                  />
+                )
+              })
+            }
+          </FormGroup>
+        </div>}
+    </div >
+  )
+
 }
-  
+
+function App() {
+
+  const [file, setFile] = useState();
+  const [data, setData] = useState();
+  const [total, setTotal] = useState();
+
+  async function getPrediction(image_file) {
+
+    // send image to backend for prediction
+    const res = await axios({
+      method: "post",
+      url: "/prediction",
+      data: image_file,
+      headers: { "Content-Type": image_file.type },
+    })
+
+    setData(res.data);
+  }
+
+  function handleChange(e) {
+    setFile(URL.createObjectURL(e.target.files[0]));
+    getPrediction(e.target.files[0])
+  }
+
+  useEffect(() => {
+    document.title = "Caloriephone"
+  }, [])
+
+  return (
+    <div className="App">
+      <ThemeProvider theme={createMuiTheme({ palette: { primary: { main: "#cbe4f9" }, secondary: { main: "#cdf5f6" } } })}>
+
+        <AppBar color="primary"><h2>Caloriephone</h2></AppBar>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: '10%'
+        }}>
+          <label htmlFor="contained-button-file">
+            <Input accept="image/*" id="contained-button-file" type="file" onChange={handleChange} style={{ display: 'none' }} />
+            <Button variant="contained" component="span" color="primary">
+              Upload
+            </Button>
+          </label>
+        </div>
+        <div className="imageWrap" style={{ marginTop: "5%" }}>
+          <img src={file} className="Image" />
+        </div>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+          {typeof data !== 'undefined' &&
+            <div style={{ width: "400px" }}>
+              <PredictionList predictions={data} ></PredictionList>
+            </div>
+          }
+        </div>
+      </ThemeProvider>
+    </div>
+
+  );
+}
+
 export default App;
